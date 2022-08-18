12d8dedf418d6e7f0db6a17e04a6fb8a86f6291b
alex <a13xlarin@gmail.com>
2022-08-18 18:36:41 +0300

    Добавлен класс для страницы продукта с методами для проверки добавления продукта в корзину

---
+++ use_pattern_Page_Object/pages/product_page.py
@@ -0,0 +1,22 @@
+from .locators import ProductPageLocators
+from .base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        busket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        busket_button.click()
+
+    def should_be_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Basket button is not presented"
+
+    def should_be_product_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Product price is not presented"
+
+    def should_same_product_title_after_basket_add_in_message(self):
+        assert self.is_texts_of_elements_equal((ProductPageLocators.PRODUCT_TITLE,
+                                                ProductPageLocators.PRODUCT_TITLE_IN_MESSAGE))
+
+    def should_same_product_price_after_basket_add_in_message(self):
+        assert self.is_texts_of_elements_equal((ProductPageLocators.PRODUCT_PRICE,
+                                                ProductPageLocators.PRODUCT_PRICE_IN_MESSAGE))
