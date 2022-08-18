df20b65f40d86eb19db58f42cbf0c7b9ef1f0e3a
alex <a13xlarin@gmail.com>
2022-08-18 18:33:17 +0300

    Добавлены локаторы для цен и названий на странице продукта

--- use_pattern_Page_Object/pages/locators.py
+++ use_pattern_Page_Object/pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "form#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "div.col-sm-6.product_main h1")
+    PRODUCT_TITLE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert:nth-child(1) > .alertinner > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main p.price_color")
+    PRODUCT_PRICE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert:nth-child(3) > .alertinner > p > strong")
