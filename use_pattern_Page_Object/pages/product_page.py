fbb8a42538b212547bfceaffe911299b9a50546a
alex <a13xlarin@gmail.com>
2022-08-18 19:36:00 +0300

    Изменен способ проверки отличий в наименовании и цене у продукта на странице

--- use_pattern_Page_Object/pages/product_page.py
+++ use_pattern_Page_Object/pages/product_page.py
@@ -11,13 +11,16 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Basket button is not presented"
 
     def should_be_product_price(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Product price is not presented"
+        assert self.is_element_present(*ProductPageLocators.PRICE), "Product price is not presented"
+
+    def should_be_product_title(self):
+        assert self.is_element_present(*ProductPageLocators.TITLE), "Product price is not presented"
 
     def should_be_same_product_title_after_basket_add(self):
-        assert self.is_text_of_elements_equal((ProductPageLocators.PRODUCT_TITLE,
-                                                ProductPageLocators.PRODUCT_TITLE_IN_MESSAGE)), "Products titles are different"
+        assert self.get_element_text(ProductPageLocators.TITLE) == \
+               self.get_element_text(ProductPageLocators.TITLE_IN_MESSAGE), "Product titles are different"
 
     def should_be_same_product_price_after_basket_add(self):
-        assert self.is_text_of_elements_equal((ProductPageLocators.PRODUCT_PRICE,
-                                                ProductPageLocators.PRODUCT_PRICE_IN_MESSAGE)), "Products prices are different"
+        assert self.get_element_text(ProductPageLocators.PRICE) == \
+               self.get_element_text(ProductPageLocators.PRICE_IN_MESSAGE), "Product prices are different"
 
