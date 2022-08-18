a0a00405f0b1cfdda9c562c8ac4e087c3a6a2089
alex <a13xlarin@gmail.com>
2022-08-18 19:42:04 +0300

    Объединены тесты в один для увеличения скорости Добавлены ссылки для тестов Добавлена метка xfail в тест с багом

--- use_pattern_Page_Object/test_product_page.py
+++ use_pattern_Page_Object/test_product_page.py
@@ -1,32 +1,25 @@
 from .pages.product_page import ProductPage
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+          pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-
-
-def test_guest_should_see_basket_button(browser):
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_basket_button()
-
-
-def test_product_title_equals_product_title_in_message(browser):
-    page = ProductPage(browser, link)
-    page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_same_product_title_after_basket_add()
-
-
-def test_product_price_equals_price_in_message(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
     page.should_be_same_product_price_after_basket_add()
 
