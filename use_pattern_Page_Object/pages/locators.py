f89f814595200a28aacbf4c87b38777b36456690
alex <a13xlarin@gmail.com>
2022-08-20 20:17:58 +0300

    Добавлены локаторы для страниц: корзина, главная. Перенесены локаторы для перехода на страницу логина в BasePageLocators

--- use_pattern_Page_Object/pages/locators.py
+++ use_pattern_Page_Object/pages/locators.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group > a.btn.btn-default")
 
 
 class LoginPageLocators:
@@ -16,3 +21,9 @@ class ProductPageLocators:
     TITLE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert:nth-child(1) > .alertinner > strong")
     PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main p.price_color")
     PRICE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert:nth-child(3) > .alertinner > p > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert:nth-child(1)")
+
+
+class BasketPageLocators:
+    EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "div#content_inner > p")
+    BASKET_SUMMARY = (By.CSS_SELECTOR, "form.basket_summary")
