8c181121671df4b0a0fbdf411153292221c6a181
alex <a13xlarin@gmail.com>
2022-08-20 20:16:05 +0300

    Добавлен класс для тестов на странице корзины Добавлены методы should_be_text_of_empty_basket_in_empty_basket и should_not_be_items_in_empty_basket

---
+++ use_pattern_Page_Object/pages/basket_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_be_text_of_empty_basket_in_empty_basket(self):
+        assert self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_TEXT).text == \
+               "Your basket is empty. Continue shopping", "Basket is empty, but text about it is wrong or missing"
+
+    def should_not_be_items_in_empty_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_SUMMARY), "Items in empty basket"
