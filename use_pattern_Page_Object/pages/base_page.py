01505e9dc60270f43983b3e23585332bc4b00911
alex <a13xlarin@gmail.com>
2022-08-20 20:15:06 +0300

    удален метод get_element_text добавлены is_not_element_present и is_disappeared перенесены из main_page - go_to_login_page, should_be_login_link так как ссылка на логин присутствует на большинстве страницах

--- use_pattern_Page_Object/pages/base_page.py
+++ use_pattern_Page_Object/pages/base_page.py
@@ -1,15 +1,17 @@
 import math
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from .locators import BasePageLocators
 
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
     def is_element_present(self, how, what):
         try:
@@ -18,11 +20,25 @@ class BasePage:
             return False
         return True
 
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            print("не пропал")
+            return False
+        print("пропал")
+        return True
+
     def open(self):
         self.browser.get(self.url)
-
-    def get_element_text(self, locator):
-        return self.browser.find_element(*locator).text
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -37,4 +53,12 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
