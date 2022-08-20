ad28317fbf468e814e7af87b8bba85cfca65689c
alex <a13xlarin@gmail.com>
2022-08-20 20:21:32 +0300

    Добавлены методы для проверки сообщения о добавленом товаре в корзину и проверки его скрытия Добавлены методы проверки и перехода на страницу корзины

--- use_pattern_Page_Object/pages/product_page.py
+++ use_pattern_Page_Object/pages/product_page.py
@@ -1,13 +1,13 @@
-from .locators import ProductPageLocators
+from .locators import ProductPageLocators, MainPageLocators
 from .base_page import BasePage
 
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
-        busket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
-        busket_button.click()
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
 
-    def should_be_basket_button(self):
+    def should_be_basket_add_button(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Basket button is not presented"
 
     def should_be_product_price(self):
@@ -17,10 +17,30 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.TITLE), "Product price is not presented"
 
     def should_be_same_product_title_after_basket_add(self):
-        assert self.get_element_text(ProductPageLocators.TITLE) == \
-               self.get_element_text(ProductPageLocators.TITLE_IN_MESSAGE), "Product titles are different"
+        assert self.browser.find_element(*ProductPageLocators.TITLE).text == \
+               self.browser.find_element(*ProductPageLocators.TITLE_IN_MESSAGE).text, "Product titles are different"
 
     def should_be_same_product_price_after_basket_add(self):
-        assert self.get_element_text(ProductPageLocators.PRICE) == \
-               self.get_element_text(ProductPageLocators.PRICE_IN_MESSAGE), "Product prices are different"
+        assert self.browser.find_element(*ProductPageLocators.PRICE).text == \
+               self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE).text, "Product prices are different"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should disappeared"
+
+    def should_be_basket_button(self):
+        assert self.is_element_present(*MainPageLocators.BASKET_BUTTON), \
+            "The Basket button is not presented"
+
+    def go_to_basket_page_by_basket_button(self):
+        self.browser.find_element(*MainPageLocators.BASKET_BUTTON).click()
+
+
+
+
+
 
