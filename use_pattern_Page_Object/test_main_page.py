1be7be3d6d9f3b84ffca8f190860ad5fa067bf3e
alex <a13xlarin@gmail.com>
2022-08-20 20:22:17 +0300

    Добавлен тест - test_guest_cant_see_product_in_basket_opened_from_main_page для проверки пустой корзины

--- use_pattern_Page_Object/test_main_page.py
+++ use_pattern_Page_Object/test_main_page.py
@@ -1,9 +1,9 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest
 
-from time import sleep
-
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
@@ -13,8 +13,22 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_basket_button()
+    page.go_to_basket_page_by_basket_button()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items_in_empty_basket()
+    basket_page.should_be_text_of_empty_basket_in_empty_basket()
+
+
