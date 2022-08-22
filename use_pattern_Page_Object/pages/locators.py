83ec85268e2dc0ba7422c0cd6ae929ec9f48dd34
alex <a13xlarin@gmail.com>
2022-08-22 19:47:23 +0300

    Добавлены локаторы для формы регистрации Добавлен локатор иконки авторизованного пользователя

--- use_pattern_Page_Object/pages/locators.py
+++ use_pattern_Page_Object/pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class MainPageLocators:
@@ -13,6 +14,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "form#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "input#id_registration-email.form-control")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "input#id_registration-password1.form-control")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "input#id_registration-password2.form-control")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[name=registration_submit]")
 
 
 class ProductPageLocators:
