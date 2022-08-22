f363b5ba3b67fab1e16ae2ec42f9994ea5661b8a
alex <a13xlarin@gmail.com>
2022-08-22 19:46:24 +0300

    Правки отступов

--- use_pattern_Page_Object/pages/basket_page.py
+++ use_pattern_Page_Object/pages/basket_page.py
@@ -5,7 +5,7 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
     def should_be_text_of_empty_basket_in_empty_basket(self):
         assert self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_TEXT).text == \
-               "Your basket is empty. Continue shopping", "Basket is empty, but text about it is wrong or missing"
+               "Your basket is empty. Continue shopping", "Basket is empty, but text about it - is wrong or missing"
 
     def should_not_be_items_in_empty_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_SUMMARY), "Items in empty basket"
