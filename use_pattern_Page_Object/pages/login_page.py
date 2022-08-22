db3b5a29254652b83cf0632a360c60b96994043c
alex <a13xlarin@gmail.com>
2022-08-22 19:48:06 +0300

    Добавлен метод для регистрации тестового пользователя

--- use_pattern_Page_Object/pages/login_page.py
+++ use_pattern_Page_Object/pages/login_page.py
@@ -1,8 +1,18 @@
+import time
+
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self):
+        email = f"{str(time.time())}@fakemail.org"
+        password = "Qwertyqazxswedc123!"
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
