5e8be6e4ef114e1474038b766539cd1fafe091d0
alex <a13xlarin@gmail.com>
2022-08-24 15:51:31 +0300

    Исправлено название теста

--- use_pattern_Page_Object/test_product_page.py
+++ use_pattern_Page_Object/test_product_page.py
@@ -61,7 +61,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', links)
-def test_user_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
